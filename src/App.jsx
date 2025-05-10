906fce0ea9a58e8e95bbd8ff967d8b1222e1da0e
Sara <saragmal550@gmail.com>
2025-05-11 00:08:30 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Footer from './components/footer/Footer.jsx';
 import Home from './pages/homePage/Home.jsx';
 import SearchPage from './pages/searchpage/SearchPage.jsx';
 import MovieDetailsPage from './pages/details/details.jsx';
+import Loginpage from'./pages/LoginPage/LoginPage.jsx';
+import SignUppage from'./pages/SignUpPage/SignUpPage.jsx';
 import ContactUs from './pages/contactUs/contactUs.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
@@ -17,7 +19,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/movie/:id" element={<MovieDetailsPage/>} />
-        
+        <Route path="/login" element={<Loginpage/>} />
+        <Route path="/signup" element={<SignUppage/>} />
         <Route path="/contact" element={<ContactUs />} />
         </Routes>
       <Footer />
