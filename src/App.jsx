da3be5e295cd82478b10da63657097e2deeff270
Fatma Rashad <132586736+fatmarashadd@users.noreply.github.com>
2025-05-10 13:58:32 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from './components/footer/Footer.jsx';
 import Home from './pages/homePage/Home.jsx';
 import SearchPage from './pages/searchpage/SearchPage.jsx';
 import MovieDetailsPage from './pages/details/details.jsx';
-
+import ContactUs from './pages/contactUs/contactUs.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css'
@@ -17,8 +17,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/movie/:id" element={<MovieDetailsPage/>} />
-
-      </Routes>
+        
+        <Route path="/contact" element={<ContactUs />} />
+        </Routes>
       <Footer />
       </Router>
     </>
